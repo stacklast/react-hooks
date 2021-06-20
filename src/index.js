e180bc4e4e7c8e58a85acfd869bfaa23bf97023b
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-06-20 15:14:01 -0500

    ADD: Tarea Memorize

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ import ReactDOM from 'react-dom';
 // import { LayoutEffect }  from './components/05-useLayoutEffect/LayoutEffect';
 // import { Memorize }  from './components/06-memos/Memorize';
 // import { MemoHook }  from './components/06-memos/MemoHook';
-import { CallbackHook }  from './components/06-memos/CallbackHook';
+// import { CallbackHook }  from './components/06-memos/CallbackHook';
+import { Padre }  from './components/07-tarea-memo/Padre';
 ReactDOM.render(
-    <CallbackHook />,
+    <Padre />,
   document.getElementById('root')
 );
 
