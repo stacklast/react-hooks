3fc5e670619a43d0bc94c9475d7a4263ac31ad70
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-06-27 08:40:47 -0500

    ADD: Optimización #1 - Listado de TODOs

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useReducer } from 'react'
 import { todoReducer } from './todoReducer';
 import { useForm } from '../../hooks/useForm';
+import { TodoList } from './TodoList';
 
 import './style.css';
-
-
 
 
 const init = ()=> {
@@ -72,28 +71,10 @@ export const TodoApp = () => {
             <hr/>
             <div className="row">
                 <div className="col-7">
-                    <ul className="list-group list-group-flush">
-                    {
-                        todos.map( ( todo, i ) => (
-                            <li 
-                                key={todo.id}
-                                className="list-group-item" 
-                            >
-                            <p 
-                                className={ `${todo.done && 'complete'}`} 
-                                onClick={ ()=> handleToggle( todo.id ) }> 
-                            { i + 1 }. { todo.desc } 
-                            </p> 
-                            <button
-                                className="btn btn-danger"
-                                onClick={ () => handleDelete( todo.id ) }
-                            >
-                                Borrar
-                            </button>
-                            </li>
-                        ))    
-                    }
-                    </ul>
+                    <TodoList 
+                        todos={todos}
+                        handleDelete={handleDelete}
+                        handleToggle={handleToggle}  />
                 </div>
 
                 <div className="col-3">
