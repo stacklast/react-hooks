dde5092650c3918d31d322cc1017fb73edd80954
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-06-27 11:52:24 -0500

    ADD:  CreateContext y useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
+
 export const MainApp = () => {
+
+    const user = {
+        id: 123,
+        name: 'Edwin',
+        email: 'edwinrock1m@hotmail.com'
+    };
+    
     return (
+        <UserContext.Provider value={ user }>
             <AppRouter />
+        </UserContext.Provider>
     )
 }
